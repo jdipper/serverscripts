3e418532f3febb407bc5b8dd658df32884f18fa4
Jack Dipper <14117126+jdipper@users.noreply.github.com>
2018-04-06 14:26:26 +0100

    Added a set up file for GitHub

---
+++ setup_git.sh
@@ -0,0 +1,21 @@
+sudo yum -y install git
+
+echo "Please specify your name:"
+read name
+echo "Please specify your email address (or GitHub email alias):"
+read alias
+echo "Please specify your email address used to log in to GitHub:"
+read email
+echo "Please specify a file in which to save the key ($HOME/.ssh/idrsa):"
+read file
+
+if [[ $file == "" ]]; then
+  file="$HOME/.ssh/idsra"
+fi
+
+git config --global user.name "$name" 
+git config --global user.email "$alias" 
+ssh-keygen -t rsa -b 4096 -C $email -f $file
+
+echo "Set up complete.  Please copy and paste your public key below into your GitHub account:"
+cat $HOME/.ssh/idrsa.pub
